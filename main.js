d3a51d2df48df85fc07d27417d5f6b0bd2ab24aa
fedPass <federica.passanante@gmail.com>
2019-11-18 18:27:01 +0100

    correzione es (ciclo for)

--- main.js
+++ main.js
@@ -3,18 +3,18 @@
 var mailList = ['mail1','mail2','mail3'];
 console.log(mailList);
 // Attraverso un prompt chiedere all'utente la sua email.
-var mail = prompt('Inserisci la tua mail per accedere');
+var mailUtente = prompt('Inserisci la tua mail per accedere');
 // Controllare se l'email dell'utente è presente nell'array e stampare un messaggio appropriato.
-if (!isNaN(mail)) {
+if (!isNaN(mailUtente)) {
     console.log('Hai inserito un numero, devi inserire una mail');
  } else {
-    for (var i = 0; i < mailList.length; i++) {
-        if (mail == mailList[i]) {
+    var mailTrovata = false;
+    for (var i = 0; (i < mailList.length) && (mailTrovata == false); i++) {
+        if (mailUtente == mailList[i]) {
+            mailTrovata = true;
             console.log("Puoi effettuare l'accesso");
-            //come faccio a bloccare il flusso?
          }  else {
              console.log('Non risulti ancora registrato. Effettua la registrazione prima di accedere');
-             //come faccio a bloccare il flusso? mi appare 3 volte così
         }
      }
  }
